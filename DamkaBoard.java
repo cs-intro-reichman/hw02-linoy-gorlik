d197d29f3308c76fffb960419f353d89fdece09d
Linoy Gorlik <154327058+linoy-gorlik@users.noreply.github.com>
2024-01-03 18:17:58 +0200

    Add files via upload

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -9,7 +9,7 @@ public class DamkaBoard {
 
         for (int i = 0; i < n; i++) {
            
-            if (i % 2 == 0) {
+            if (i % 2 != 0) {
                 System.out.print(" ");
             }
 
